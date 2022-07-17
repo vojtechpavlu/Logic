4cf3917b9a0c1dae5a173ec8fa4c3cb5a2fed538
vojtechpavlu <vojtech.pavlu.dev>
2022-07-18 00:10:24 +0200

    Test of the basic operators

---
+++ scripts/tests/test_basic_operators.py
@@ -0,0 +1,43 @@
+import unittest
+
+from scripts.src.operators import *
+from scripts.src.term import Constant
+
+
+class TestOperators(unittest.TestCase):
+
+    def setUp(self):
+        self.t = Constant(True)
+        self.f = Constant(False)
+
+    def test_negation(self):
+        self.assertEqual(True,  Negation([self.f]).evaluate())
+        self.assertEqual(False, Negation([self.t]).evaluate())
+
+    def test_conjunction(self):
+        self.assertEqual(False, Conjunction([self.f, self.f]).evaluate())
+        self.assertEqual(False, Conjunction([self.f, self.t]).evaluate())
+        self.assertEqual(False, Conjunction([self.t, self.f]).evaluate())
+        self.assertEqual(True,  Conjunction([self.t, self.t]).evaluate())
+
+    def test_disjunction(self):
+        self.assertEqual(False, Disjunction([self.f, self.f]).evaluate())
+        self.assertEqual(True,  Disjunction([self.f, self.t]).evaluate())
+        self.assertEqual(True,  Disjunction([self.t, self.f]).evaluate())
+        self.assertEqual(True,  Disjunction([self.t, self.t]).evaluate())
+
+    def test_implication(self):
+        self.assertEqual(True,  Implication([self.f, self.f]).evaluate())
+        self.assertEqual(True,  Implication([self.f, self.t]).evaluate())
+        self.assertEqual(False, Implication([self.t, self.f]).evaluate())
+        self.assertEqual(True,  Implication([self.t, self.t]).evaluate())
+
+    def test_equivalence(self):
+        self.assertEqual(True,  Equivalence([self.f, self.f]).evaluate())
+        self.assertEqual(False, Equivalence([self.f, self.t]).evaluate())
+        self.assertEqual(False, Equivalence([self.t, self.f]).evaluate())
+        self.assertEqual(True,  Equivalence([self.t, self.t]).evaluate())
+
+
+
+
