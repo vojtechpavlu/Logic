2db8a543179ef8a5213e92f2407f57287516fac2
vojtechpavlu <vojtech.pavlu.dev>
2022-07-19 00:31:03 +0200

    DisjunctionCommutativity definition

--- scripts/src/axiom_transformation/simple_commutativity.py
+++ scripts/src/axiom_transformation/simple_commutativity.py
@@ -42,4 +42,38 @@ class ConjunctionCommutativity(AxiomTransformation):
         return term
 
 
+class DisjunctionCommutativity(AxiomTransformation):
+    """Commutativity of disjunction is one of the most basic axioms of
+    propositional logic.
+
+    Basically it says the terms of the relation can be switched, resulting
+    always the same. Formally, it can be stated that:
+
+        -------------------
+        | φ ∨ ψ <=> ψ ∨ φ |
+        |_________________|
+
+    This axiom can be applied on disjunction only.
+    """
+
+    def can_be_applied(self, term: term_module.Term) -> bool:
+        """Returns if the given term is type of disjunction, because this
+        axiom transformation can be applied on disjunction terms only.
+        """
+        return isinstance(term, Disjunction)
+
+    def apply(self, term: term_module.Term) -> term_module.Term:
+        """If the term can be transformed using this axiom, it returns a
+        brand new instance of disjunction with reversed deep copies of the
+        terms of the original disjunction. Else, it returns the original
+        term intact.
+        """
+        if self.can_be_applied(term):
+
+            # Return a brand new disjunction with reversed copies of terms
+            return Disjunction(
+                reversed(list(map(lambda t: t.clone, term.terms))))
+
+        # Returns the intact original term
+        return term
 
