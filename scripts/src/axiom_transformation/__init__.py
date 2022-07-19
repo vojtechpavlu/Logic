7d6c937a108492e69650f0928badd91e0fbf9595
vojtechpavlu <vojtech.pavlu.dev>
2022-07-19 18:40:58 +0200

    __init__ shorthand import

--- scripts/src/axiom_transformation/__init__.py
+++ scripts/src/axiom_transformation/__init__.py
@@ -2,3 +2,4 @@ from .axiom_transformation import AxiomTransformation
 from .double_negation import DoubleNegation
 from .simple_commutativity import (
     ConjunctionCommutativity, DisjunctionCommutativity, Equivalence)
+from .idempotence import ConjunctionIdempotence, DisjunctionIdempotence
